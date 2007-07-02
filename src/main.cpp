66cb9a64a61e38a340ddc239eeba7947f5296e5b
Chusslove Illich <caslav.ilic@gmx.net>
2007-07-02 08:52:55 +0000

    Auto-conversion to new KCmdLineArgs/KAboutData/KComponentData API (trunk/KDE). Watch for the few remaining QString to QByteArray assignments that will not compile without extra specification (typically needs .toLocal8Bit() or .toUtf8()).
    svn path=/trunk/KDE/kdegames/ksquares/; revision=682260

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,6 @@
 #include "ksquareswindow.h"
 #include "ksquaresdemowindow.h"
 
-static const KCmdLineOptions options[] =
-{
-	{ "demo", I18N_NOOP("Run game in demo (autoplay) mode"), 0 },
-	KCmdLineLastOption // End of options.
-};
-
 static const char description[] =
 	I18N_NOOP("Take it in turns to draw lines.\nIf you complete a squares, you get another go.");
 
@@ -30,13 +24,16 @@ static const char version[] = "0.3";
 
 int main(int argc, char **argv)
 {
-	KAboutData about("ksquares", I18N_NOOP("KSquares"), version, description,
-		KAboutData::License_GPL, "(C) 2006-2007 Matt Williams", 0, 
+	KAboutData about("ksquares", 0, ki18n("KSquares"), version, ki18n(description),
+		KAboutData::License_GPL, ki18n("(C) 2006-2007 Matt Williams"), KLocalizedString(), 
 		"http://milliams.com/content/view/18/42/");
-	about.addAuthor( "Matt Williams", I18N_NOOP("Original creator and maintainer"), "matt@milliams.com", "http://milliams.com" );
-	about.addCredit("Fela Winkelmolen", I18N_NOOP("Many patches and bugfixes"));
+	about.addAuthor( ki18n("Matt Williams"), ki18n("Original creator and maintainer"), "matt@milliams.com", "http://milliams.com" );
+	about.addCredit(ki18n("Fela Winkelmolen"), ki18n("Many patches and bugfixes"));
 	
 	KCmdLineArgs::init(argc, argv, &about);
+
+	KCmdLineOptions options;
+	options.add("demo", ki18n("Run game in demo (autoplay) mode"));
 	KCmdLineArgs::addCmdLineOptions( options );
 	KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
 	
